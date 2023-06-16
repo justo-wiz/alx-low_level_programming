ce04b7c7d57df992d77a2afc0fef64f622add6d6
Justus Mwendwa <justomwesh3@gmail.com>
2023-06-16 09:38:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - printing line with put function
+*
+*Return: 0 - sucess
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
