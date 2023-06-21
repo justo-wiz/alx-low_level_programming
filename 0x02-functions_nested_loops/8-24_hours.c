07635f24b31d1c7f9891a3259c6f630a960cf856
Justus Mwendwa <justomwesh3@gmail.com>
2023-06-20 17:57:05 -0700

    Longest day in my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,6 @@
 
 void jack_bauer(void)
 {
-
 	int a, b, c, d;
 
 	for (a = 0; a <= 2 ; a++)
@@ -18,7 +17,7 @@ void jack_bauer(void)
 	{
 	for (c = 0; c <= 5; c++)
 	{
-	for (d = 0; d <= 0; d++)
+	for (d = 0; d <= 9; d++)
 	{
 	_putchar(a + '0');
 	_putchar(b + '0');
@@ -27,7 +26,7 @@ void jack_bauer(void)
 	_putchar(d + '0');
 	_putchar('\n');
 	}
-	}	
+	}
 	}
 	}
 	}
