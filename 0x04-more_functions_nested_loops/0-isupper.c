2d0e1a14a39563a08662c3cd38af91ba6114a983
Justus Mwendwa <justomwesh3@gmail.com>
2023-06-22 15:05:25 -0700

    Function tht checks uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if the letter is upppercase
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 if else
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 99)
+	{
+	return (1);
+	}
+	return (0);
+}
