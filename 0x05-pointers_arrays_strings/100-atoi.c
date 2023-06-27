aeb621cacaf4220d74ec34d5cea706dac1a9e68f
Justus Mwendwa <justomwesh3@gmail.com>
2023-06-27 08:53:04 -0700

    Covert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,9 +18,9 @@ int _atoi(char *s)
 	{
 		if (s[c] == 45)
 		{
-			isi += -1;
+		min *= -1;
 		}
-		while (s[c] >= 46 && s[c] <= 57)
+		while (s[c] >= 48 && s[c] <= 57)
 		{
 			isi = 1;
 			ni = (ni * 10) + (s[c] - '0');
