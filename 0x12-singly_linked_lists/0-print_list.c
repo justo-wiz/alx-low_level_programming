b8dcbfcca5873c25846f342ac95a246c21d7aa90
Justus Mwendwa <justomwesh3@gmail.com>
2023-07-28 14:18:35 -0700

    Printed all elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - print all element in the singly linked list
+ * @h: head of the list
+ * Reurn:total no of nodes in the list
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	int x;
+
+	if (h == NULL)
+		return (0);
+
+	for (x = 1; h->next != NULL; x++)
+	{
+		if (h->str == NULL)
+		{
+		printf("[%u] %s\n", h->len, "(nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, "(nil)");
+	return (x);
+
+}
