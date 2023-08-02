71a3d9e9ee1411083fc6e9be16d2ddf294d9e8f1
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-02 08:44:09 -0700

    print

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
