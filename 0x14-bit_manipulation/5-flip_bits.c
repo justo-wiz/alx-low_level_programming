d85b30e74580ef1531c1befe1ae35ce9ecab4ee5
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-04 04:14:57 -0700

    Flipped

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,23 +5,23 @@
  * to get from one no to another
  *
  * @n: initial number
-  * @m: second number
+ * @m: second number
  *
  * Return: number of bits to change
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int x, c = 0;
-	/**unsigned long int now;**/
-	unsigned long int ex = n ^ m;
+	/**int x, c = 0;**/
 
-	for (x = 63; x >= 0; x--)
+	/**unsigned long int now;**/
+	unsigned long int ex = n ^ m, c = 0;
+
+
+	while (ex > 0)
 	{
-		if((ex >> x) & 1)
-		/**now = ex >> x;**/
-		/**if (now & 1)**/
-			c++;
+		c += (ex & 1);
+		ex >>= 1;
 	}
 	return (c);
 }
